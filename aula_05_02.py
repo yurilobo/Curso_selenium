c741be26d97ae390f3905638aa9861146af97668
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2020-12-10 09:33:52 -0300

    Aula 05 03

--- aula_05_02.py
+++ aula_05_02.py
@@ -1,5 +1,13 @@
 from selenium.webdriver import Firefox
+url = 'http://selenium.dunossauro.live/aula_05_a.html'
 
 firefox = Firefox()
 
-firefox.get('http://selenium.dunossauro.live/aula_05_a.html')
+firefox.get(url)
+
+topico =firefox.find_element_by_class_name('topicos')
+linguagens =firefox.find_element_by_class_name(  'linguagens')
+
+for linguagen in linguaens:
+    print(linguagen.find_element_by_tag_name('h2').text,
+    linguagen.find_element_by_tag_name('p').text)
